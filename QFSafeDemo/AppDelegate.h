ec9d8d39eb914f9d6aa1593c776c8f621222706a
流年释残忆 <291753906@qq.com>
2018-03-15 21:07:40 +0800

    Initial Commit

---
+++ QFSafeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QFSafeDemo
+//
+//  Created by liyiping on 2018/3/15.
+//  Copyright © 2018年 情风. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QFSafeDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  QFSafeDemo
+//
+//  Created by liyiping on 2018/3/15.
+//  Copyright © 2018年 情风. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
